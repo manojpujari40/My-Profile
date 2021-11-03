eae95d541aa2038652cc4b9f607dd76a2508ab48
manojpujari40 <50852168+manojpujari40@users.noreply.github.com>
2021-11-03 21:48:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function red(){
+    document.getElementById("folder").style.backgroundColor="red"
+}
+function yellow(){
+    document.getElementById("folder").style.backgroundColor="yellow"
+}
+function cyan(){
+    document.getElementById("folder").style.backgroundColor="cyan"
+}
